20b8e5747e5efb195292acd215246cfe67af19b9
Hubert Rykała <94188186+hubertrykala93@users.noreply.github.com>
2022-06-14 16:14:37 +0200

    Update exceptions.py

--- python/coffee_shop/exceptions.py
+++ python/coffee_shop/exceptions.py
@@ -1,1 +1,17 @@
+class ProductNotAvailable(Exception):
+    def __init__(self, product, message=' is currently unavailable!'):
+        self.message = message
+        self.product = product
 
+        super(ProductNotAvailable, self).__init__(message, product)
+
+    def __str__(self):
+        return self.product.title() + self.message
+
+
+class OrderFulfilled(Exception):
+    def __init__(self, message='All orders have been fulfilled!'):
+        self.message = message
+
+    def __str__(self):
+        return self.message
